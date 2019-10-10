24b05dc52c099c42b79c5445de033af15128b0d1
1894082 <ymodi3859@gmail.com>
2019-10-10 16:39:40 -0400

    Added no Connectivity 'WEBPAGE'

--- app/src/main/java/com/example/webprogress/MainActivity.java
+++ app/src/main/java/com/example/webprogress/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.webprogress;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webview);
-        progressDialog=new ProgressDialog(MainActivity.this);
+        progressDialog = new ProgressDialog(MainActivity.this);
 
         String link = "https://www.bbc.com/weather/6077243";
-
 
 
         webView.setWebViewClient(new myWebView());
@@ -33,12 +36,26 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setLoadsImagesAutomatically(true);
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         webView.getSettings().setDomStorageEnabled(true);
-        webView.loadUrl(link);
+
+        if (isConnected() == true) {
+            webView.loadUrl(link);
+        } else {
+            webView.loadUrl("file:///android_res/raw/test.htm");
+            Toast.makeText(getApplicationContext(), "Check Internet Connectivity", Toast.LENGTH_LONG).show();
+        }
 
 
     }
 
-    public class myWebView extends WebViewClient{
+
+    public boolean isConnected() {
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        return info != null && info.isConnectedOrConnecting();
+    }
+
+    public class myWebView extends WebViewClient {
 
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
